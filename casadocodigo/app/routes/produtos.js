b9b7ce5d63cd31e33a8d7409498277b1b193abe5
Marcus Santos <marcusv.bianchisantos@gmail.com>
2017-06-23 16:05:05 -0300

    Node course

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,12 +1,32 @@
 module.exports = function(app) {
   app.get("/produtos", function(req, res) {
     var connection = app.infra.connectionFactory();
-
-    connection.query("select * from produtos", function(err, results) {
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.lista(function(err, results) {
       if (err) console.log(err);
-      res.render("produtos/lista", { lista: results });
+      res.format({
+        html: function() {
+          res.render("produtos/lista", { lista: results });
+        },
+        json: function() {
+          res.json(results);
+        }
+      });
     });
-
     connection.end();
+  });
+
+  app.get("/produtos/form", function(req, res) {
+    res.render("produtos/form");
+  });
+
+  app.post("/produtos", function(req, res) {
+    var produto = req.body;
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.salva(produto, function(err, resultados) {
+      if (err) console.log(err);
+      res.redirect("/produtos");
+    });
   });
 };
