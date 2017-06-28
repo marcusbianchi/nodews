15f80bb44112fcb10cd0c111206f5852e65f07b8
Marcus Santos <marcusv.bianchisantos@gmail.com>
2017-06-28 09:34:53 -0300

    Addition of unit testing

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     var connection = app.infra.connectionFactory();
     var produtosDAO = new app.infra.ProdutosDAO(connection);
     produtosDAO.lista(function(err, results) {
-      if (err) console.log(err);
+      if (err) res.send(err);
       res.format({
         html: function() {
           res.render("produtos/lista", { lista: results });
@@ -17,11 +17,29 @@ module.exports = function(app) {
   });
 
   app.get("/produtos/form", function(req, res) {
-    res.render("produtos/form");
+    res.render("produtos/form", { errosValidacao: {}, produto: {} });
   });
 
   app.post("/produtos", function(req, res) {
     var produto = req.body;
+    req.assert("titulo", "Titulo é obrigatório").notEmpty();
+    req.assert("preco", "Formato Invalido").isFloat();
+    var erros = req.validationErrors();
+    if (erros) {
+      res.format({
+        html: function() {
+          res.render("produtos/form", {
+            errosValidacao: erros,
+            produto: produto
+          });
+        },
+        json: function() {
+          res.status(400).json(erros);
+        }
+      });
+      return;
+    }
+
     var connection = app.infra.connectionFactory();
     var produtosDAO = new app.infra.ProdutosDAO(connection);
     produtosDAO.salva(produto, function(err, resultados) {
