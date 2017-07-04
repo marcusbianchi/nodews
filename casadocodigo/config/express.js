135457d75f4d46642315598dd050b8d7f02cb94f
Marcus Santos <marcusv.biancisantos@gmail.com>
2017-07-04 15:57:14 -0300

    Course End

--- casadocodigo/config/express.js
+++ casadocodigo/config/express.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var load = require('express-load');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
+var express = require("express");
+var load = require("express-load");
+var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
 
 module.exports = function() {
-    var app = express();
-    app.use(express.static('./app/public'));
+  var app = express();
+  app.use(express.static("./app/public"));
 
+  app.set("view engine", "ejs");
+  app.set("views", "./app/views");
 
-    app.set('view engine', 'ejs');
-    app.set('views', './app/views');
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(expressValidator());
 
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    app.use(expressValidator());
+  load("routes", { cwd: "app" }).then("infra").into(app);
 
-    load('routes', {cwd: 'app'})
-        .then('infra')
-        .into(app);
+  app.use(function(req, res, next) {
+    res.status(404).render("erros/404");
+  });
 
-    return app;
-}
+  app.use(function(error, req, res, next) {
+    if (process.env.NODE_ENV == "production") {
+      res.status(500).render("errors/500");
+      return;
+    }
+    next(errors);
+  });
+  //tem que colocar na ordem, caso contrário ele passa pelo middleware e
+  //ainda não vai ter acontecido nenhum erro.
+
+  return app;
+};
