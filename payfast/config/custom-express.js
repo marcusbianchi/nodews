7a53db989ee1492922daf0eb98169516fe9da452
Marcus Santos <marcusv.bianchisantos@gmail.com>
2017-07-13 09:46:36 -0300

    End of second node class

--- payfast/config/custom-express.js
+++ payfast/config/custom-express.js
@@ -2,10 +2,11 @@ var express = require("express");
 var app = express();
 var consign = require("consign");
 var bodyParser = require("body-parser");
-
+var expressValidator = require("express-validator");
 module.exports = function() {
+  app.use(expressValidator());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  consign().include("controllers").into(app);
+  consign().include("controllers").then("persistencia").into(app);
   return app;
 };
--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -3,9 +3,45 @@ module.exports = function(app) {
     console.log("Recebida Requisição");
     res.send("Ok.");
   });
+
   app.post("/pagamentos/pagamento", function(req, res) {
     var pagamento = req.body;
-    console.log(pagamento);
-    res.send("Ok.");
+    console.log("processando pagamento...");
+
+    var connection = app.persistencia.connectionFactory();
+    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+    req
+      .assert("forma_de_pagamento", "Forma de pagamento é obrigatória.")
+      .notEmpty();
+    req
+      .assert("valor", "Valor é obrigatório e deve ser um decimal.")
+      .notEmpty()
+      .isFloat();
+    req
+      .assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres")
+      .notEmpty()
+      .len(3, 3);
+
+    if (errors) {
+      console.log("Erros de validação encontrados");
+      res.status(400).send(errors);
+      return;
+    }
+
+    var errors = req.validationErrors();
+
+    pagamento.status = "CRIADO";
+    pagamento.data = new Date();
+
+    pagamentoDao.salva(pagamento, function(exception, result) {
+      if (erro) {
+        res.status(500).send(erro);
+      }
+      res.location("/pagamentos/pagamento/" + resultado.insertId);
+      pagamento.id = resultado.insertId;
+
+      console.log("pagamento criado: " + result);
+      res.status(201).json(pagamento);
+    });
   });
 };
